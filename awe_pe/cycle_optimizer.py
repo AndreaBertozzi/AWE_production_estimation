Add scaling to constraints and fix constraints in hybrid mode

awe_pe/cycle_optimizer.py
@@ -511,21 +511,21 @@ class OptimizerCycle(Optimizer):
         # Common constraints
         # Constraint on the number of cross-wind patterns. It is assumed that a realistic reel-out trajectory should
         # include at least one crosswind pattern.
-        ineq_cons_cw_patterns = res["n_crosswind_patterns"] - self.parametric_cons_values[0]
+        ineq_cons_cw_patterns = (res["n_crosswind_patterns"] - self.parametric_cons_values[0])*1e-1
 
         # Minimum tether length
-        ineq_cons_min_tether_length = -res["min_tether_length"] + x_real_scale[6]
+        ineq_cons_min_tether_length = (-res["min_tether_length"] + x_real_scale[6])*1e-1
 
         # Stroke + minimum length is less than physical tether length
         ineq_cons_max_tether_length = (self.system_properties.total_tether_length 
-                                         - x_real_scale[6] - x_real_scale[5])
+                                         - x_real_scale[6] - x_real_scale[5])*1e-1
 
         # Overflying ground station
         ineq_cons_max_elevation = self.parametric_cons_values[1] - res['max_elevation_angle']
 
         ineq_cons_max_course_rate = self.parametric_cons_values[2] - res['max_course_rate']
 
-        ineq_cons_min_height =  - self.parametric_cons_values[3] + res['min_height'] 
+        ineq_cons_min_height =  (-self.parametric_cons_values[3] + res['min_height'])*1e-2
 
         if self.force_or_speed_control == 'force':
             # When speed limits are active during the optimization (see determine_new_steady_state method of Phase
@@ -549,6 +549,7 @@ class OptimizerCycle(Optimizer):
                                     ineq_cons_max_elevation, ineq_cons_max_course_rate, ineq_cons_min_height])        
                     
         elif self.force_or_speed_control == 'speed':
+            # TODO: FIX CONSTRAINTS
             # When force limits are active during the optimization (see determine_new_steady_state method of Phase
             # class in qsm.py), the setpoint reel-out/reel-in speeds are overruled. For special cases, the respective
             # optimization variables won't affect the simulation. The lower constraints avoid random steps between
@@ -578,20 +579,26 @@ class OptimizerCycle(Optimizer):
             if res['max_reeling_speed']['out'] == np.inf:
                 res['max_reeling_speed']['out'] = 0.
                 
-            speed_out_setpoint_max = -(res['max_reeling_speed']['out'] - x_real_scale[0])*1e-2 + 1e-6
-            force_in_setpoint_max = (res['max_tether_force']['in'] - x_real_scale[1])*1e-2 + 1e-6
+            speed_out_setpoint_max = -(res['max_reeling_speed']['out'] - x_real_scale[0])*1e-1 + 1e-6
+            speed_out_setpoint_min = (res['min_reeling_speed']['out'] - x_real_scale[0])*1e-1 + 1e-6
+            force_in_setpoint_max = (res['max_tether_force']['in'] - x_real_scale[1])*1e-3 + 1e-6
+            speed_in = -res['max_reeling_speed']['in']
             
             # The maximum reel-out speed can be exceeded when the tether speed control is overruled by the maximum
             # reel-out speed limit and the imposed reel-out force yields a reel-out speed exceeding its set point. This
             # scenario is prevented by the lower constraint.
             speed_max_limit = self.system_properties.reeling_speed_max_limit
             max_speed_violation_traction = res['max_reeling_speed']['out'] - speed_max_limit
+
+            force_max_limit = self.system_properties.tether_force_max_limit
+            max_force_violation_traction = -(res['max_tether_force']['out'] - force_max_limit)*1e-3 + 1e-6
+    
             ineq_cons_traction_max_speed = -max_speed_violation_traction/(speed_max_limit)            
             
-            ineq_cons = np.array([speed_out_setpoint_max, force_in_setpoint_max, ineq_cons_traction_max_speed,
+            ineq_cons = np.array([speed_out_setpoint_max, speed_out_setpoint_min, force_in_setpoint_max, max_force_violation_traction,
+                                   speed_in ,ineq_cons_traction_max_speed,
                                   ineq_cons_cw_patterns, ineq_cons_min_tether_length, ineq_cons_max_tether_length,
-                                    ineq_cons_max_elevation, ineq_cons_max_course_rate, ineq_cons_min_height])
-
+                                    ineq_cons_max_elevation, ineq_cons_max_course_rate, ineq_cons_min_height])           
         return obj, ineq_cons
 
     def eval_performance_indicators(self, x_real_scale, plot_result=False, relax_errors=True, labels=None):
@@ -671,6 +678,9 @@ class OptimizerCycle(Optimizer):
                 'trans': cycle.transition_phase.max_tether_force,
                 'out': cycle.traction_phase.max_tether_force,
             },
+            'mean_tether_force': {
+                'out': 0.5*(cycle.traction_phase.max_tether_force + cycle.traction_phase.min_tether_force),
+            },            
             'min_reeling_speed': {
                 'in': cycle.retraction_phase.min_reeling_speed,
                 'out': cycle.traction_phase.min_reeling_speed,